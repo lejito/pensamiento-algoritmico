d7eb69bbd5ddd1b10a9290dfaa40b54810d8f595
Lejito <alejocrrs@gmail.com>
2024-04-10 00:00:34 -0500

    Refactor SectionsList and Aside component

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,74 +1,39 @@
+"use client";
+import { sections } from "@/utils/sections";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export function Aside() {
+  const pathname = usePathname();
+
   return (
     <aside className="hidden lg:block fixed z-20 inset-0 top-16 w-80 pt-6 pb-10 pl-10 pr-6 overflow-y-auto border-r border-slate-300 dark:border-slate-700">
       <nav className="relative">
-        <ul>
-          <li>
-            <h2 className="mt-4 font-semibold text-slate-700 dark:text-slate-100">Introducción</h2>
-            <ul>
-              <li className="block py-2 px-6  my-1 transition-colors text-slate-900 hover:text-slate-900 border-l border-slate-500 hover:border-slate-400 dark:text-slate-100 dark:hover:text-slate-100 dark:border-slate-100 dark:hover:border-slate-100">
-                <Link href="/core/introduction/que-es-la-logica">
-                  ¿Qué es la lógica?
-                </Link>
-              </li>
-              <li className="block py-2 px-6  my-1 transition-colors text-slate-500 hover:text-slate-900 border-l border-slate-200 hover:border-slate-400 dark:text-slate-400 dark:hover:text-slate-300 dark:border-slate-400 dark:hover:border-slate-300">
-                <a href="/core/colors">Colors</a>
-              </li>
-              <li className="block py-2 px-6  my-1 transition-colors text-slate-500 hover:text-slate-900 border-l border-slate-200 hover:border-slate-400 dark:text-slate-400 dark:hover:text-slate-300 dark:border-slate-400 dark:hover:border-slate-300">
-                <a href="/core/typography">Typography</a>
-              </li>
-              <li className="block py-2 px-6  my-1 transition-colors text-slate-500 hover:text-slate-900 border-l border-slate-200 hover:border-slate-400 dark:text-slate-400 dark:hover:text-slate-300 dark:border-slate-400 dark:hover:border-slate-300">
-                <a href="/core/shadows">Shadows</a>
-              </li>
-            </ul>
-          </li>
-
-          <li>
-            <h2 className="mt-4 font-semibold text-slate-700 dark:text-slate-100">Components</h2>
-            <ul>
-              <li className="block py-2 px-6  my-1 transition-colors text-slate-500 hover:text-slate-900 border-l border-slate-200 hover:border-slate-400 dark:text-slate-400 dark:hover:text-slate-300 dark:border-slate-400 dark:hover:border-slate-300">
-                <a href="/components/buttons">Buttons</a>
-              </li>
-              <li className="block py-2 px-6  my-1 transition-colors text-slate-500 hover:text-slate-900 border-l border-slate-200 hover:border-slate-400 dark:text-slate-400 dark:hover:text-slate-300 dark:border-slate-400 dark:hover:border-slate-300">
-                <a href="/components/input">Input</a>
-              </li>
-              <li className="block py-2 px-6  my-1 transition-colors text-slate-500 hover:text-slate-900 border-l border-slate-200 hover:border-slate-400 dark:text-slate-400 dark:hover:text-slate-300 dark:border-slate-400 dark:hover:border-slate-300">
-                <a href="/components/status-pill">Status pills</a>
-              </li>
-              <li className="block py-2 px-6  my-1 transition-colors text-slate-500 hover:text-slate-900 border-l border-slate-200 hover:border-slate-400 dark:text-slate-400 dark:hover:text-slate-300 dark:border-slate-400 dark:hover:border-slate-300">
-                <a href="/components/table">Table</a>
-              </li>
-            </ul>
-          </li>
-
-          <li>
-            <h2 className="mt-4 font-semibold text-slate-700 dark:text-slate-100">Components</h2>
-            <ul>
-              <li className="block py-2 px-6  my-1 transition-colors text-slate-500 hover:text-slate-900 border-l border-slate-200 hover:border-slate-400 dark:text-slate-400 dark:hover:text-slate-300 dark:border-slate-400 dark:hover:border-slate-300">
-                <a href="/components/buttons">Buttons</a>
-              </li>
-              <li className="block py-2 px-6  my-1 transition-colors text-slate-500 hover:text-slate-900 border-l border-slate-200 hover:border-slate-400 dark:text-slate-400 dark:hover:text-slate-300 dark:border-slate-400 dark:hover:border-slate-300">
-                <a href="/components/input">Input</a>
-              </li>
-              <li className="block py-2 px-6  my-1 transition-colors text-slate-500 hover:text-slate-900 border-l border-slate-200 hover:border-slate-400 dark:text-slate-400 dark:hover:text-slate-300 dark:border-slate-400 dark:hover:border-slate-300">
-                <a href="/components/status-pill">Status pills</a>
-              </li>
-              <li className="block py-2 px-6  my-1 transition-colors text-slate-500 hover:text-slate-900 border-l border-slate-200 hover:border-slate-400 dark:text-slate-400 dark:hover:text-slate-300 dark:border-slate-400 dark:hover:border-slate-300">
-                <a href="/components/table">Table</a>
-              </li>
-            </ul>
-          </li>
-
-          <li>
-            <h2 className="mt-4 font-semibold text-slate-700 dark:text-slate-100">Patterns</h2>
-            <ul>
-              <li className="block py-2 px-6  my-1 transition-colors text-slate-500 hover:text-slate-900 border-l border-slate-200 hover:border-slate-400 dark:text-slate-400 dark:hover:text-slate-300 dark:border-slate-400 dark:hover:border-slate-300">
-                <a href="/patterns/introduction">Introduction</a>
-              </li>
-            </ul>
-          </li>
+        <ul className="pt-6">
+          {sections.map((section) => (
+            <li key={sections.indexOf(section) + 1} className="pb-5">
+              <h2 className="mb-4 font-semibold text-lg leading-5 text-slate-950 dark:text-slate-50">
+                {section.title}
+              </h2>
+              <ul>
+                {section.pages &&
+                  section.pages.map((page) => (
+                    <li key={section.pages.indexOf(page) + 1}>
+                      <Link
+                        href={"/ruta" + section.href + page.href}
+                        className={`block py-2 px-6 my-2 transition-colors border-l duration-300 ${
+                          pathname === "/ruta" + section.href + page.href
+                            ? "border-slate-700 text-slate-950 dark:border-slate-200 dark:text-slate-200"
+                            : "border-slate-300 text-slate-400 dark:border-slate-500 dark:text-slate-500 hover:border-slate-500 hover:text-slate-600 dark:hover:border-slate-400 dark:hover:text-slate-400"
+                        }`}
+                      >
+                        {page.title}
+                      </Link>
+                    </li>
+                  ))}
+              </ul>
+            </li>
+          ))}
         </ul>
       </nav>
     </aside>
---
+++ src/utils/sections.ts
@@ -0,0 +1,93 @@
+import { title } from "process";
+
+export const sections = [
+  {
+    title: "Introducción",
+    description:
+      "Una breve introducción a la lógica de resolución de problemas.",
+    image: "/1-introduccion.jpg",
+    href: "/introduccion",
+    pages: [
+      {
+        title: "¿Qué es la lógica?",
+        href: "/que-es-la-logica",
+      },
+      {
+        title: "¿Qué es un problema?",
+        href: "/que-es-un-problema",
+      },
+      {
+        title: "Solución de problemas",
+        href: "/solucion-de-problemas",
+      },
+      {
+        title: "Métodos de solución de problemas",
+        href: "/metodos-de-solucion-de-problemas",
+      }
+    ],
+  },
+  {
+    title: "Algoritmos",
+    description:
+      "Conceptos básicos de los algoritmos y el paso a paso para implementarlos.",
+    image: "/2-algoritmos.jpg",
+    href: "/ruta/algoritmos",
+    pages: [
+      {
+        title: "¿Qué es un algoritmo?",
+        href: "/que-es-un-algoritmo",
+      },
+      {
+        title: "Características de un algoritmos",
+        href: "/caracteristicas-de-un-algoritmos",
+      },
+      {
+        title: "Partes de un algoritmo",
+        href: "/partes-de-un-algoritmo",
+      },
+      {
+        title: "Etapas para la creación de un algoritmo",
+        href: "/etapas-para-la-creacion-de-un-algoritmo",
+      },
+      {
+        title: "Medios de expresión de un algoritmo",
+        href: "/medios-de-expresion-de-un-algoritmo",
+      }
+    ],
+  },
+  {
+    title: "Programas",
+    description:
+      "Cómo se estructuran los programas, su ciclo de vida y una pequeña introducción a los lenguajes de programación.",
+    image: "/3-programas.jpg",
+    href: "/ruta/programas",
+  },
+  {
+    title: "Herramientas de programación",
+    description:
+      "Los tipos de datos, identificadores, operadores y expresiones principales y cómo se utilizan en Python.",
+    image: "/4-herramientas-de-programacion.jpg",
+    href: "/ruta/herramientas-de-programacion",
+  },
+  {
+    title: "Estructuras de control",
+    description:
+      "Estructuras de control de flujo como las secuenciales, condicionales y repetitivas",
+    image: "/5-estructuras-de-control.jpg",
+    href: "/ruta/estructuras-de-control",
+  },
+  {
+    title: "Modularidad y funciones",
+    description:
+      "La modularidad y las funciones como herramientas para organizar y reutilizar código.",
+    image: "/6-modularidad-y-funciones.jpg",
+    href: "/ruta/modularidad-y-funciones",
+  },
+  {
+    title: "Arreglos",
+    description:
+      "Los arreglos, tipos de arreglos y formas de operar con ellos.",
+    image: "/7-arreglos.jpg",
+    href: "/ruta/arreglos",
+  },
+];
